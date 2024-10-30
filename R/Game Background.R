90ed73440242ffe3e1cdaec0769ea9736f4c38bb
Joseph Hefner <Hefnerjoseph87@gmail.com>
2024-10-30 13:52:59 -0700

    Update Game Background.R

--- R/Game Background.R
+++ R/Game Background.R
@@ -99,7 +99,7 @@ background_function <- function(f_sched = sched,
   )
   
   ggsave(p, 
-         filename = paste0("./images/Background.png"), 
+         filename = paste0("../images/Background.png"), 
          height = 12, 
          width = 10)
   
